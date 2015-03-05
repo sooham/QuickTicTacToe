607f8a71e6521fd801c6ae0d2d9dacdbe6b12dfd
Sooham Rafiz <rafizsooham@gmail.com>
2015-03-05 17:00:36 -0500

    Added support for Python 2 versions, now can run on Mac OS default python

--- game_view.py
+++ game_view.py
@@ -1,3 +1,4 @@
+import sys
 class GameView:
     '''
     A game view for a two-player, sequential move, zero-sum,
@@ -24,36 +25,35 @@ class GameView:
 
         Play a game.
         '''
-        print(self.state.instructions)
-        print(self.state)
-        print()
+        print self.state.instructions
+        print self.state
+        print '\n'
         while self.state.possible_next_moves():
             if self.state.next_player == 'p1':
                 m = self.state.get_move()
                 while m not in self.state.possible_next_moves():
                     # The move was illegal.
-                    print('Illegal move: {}\nPlease try again.\n'.format(m))
-                    print(self.state.instructions)
-                    print(self.state)
+                    print 'Illegal move: {}\nPlease try again.\n'.format(m)
+                    print self.state.instructions
+                    print self.state
                     m = self.state.get_move()
-                print('You choose: {}'.format(m))
+                print 'You choose: {}'.format(m)
             else:
                 # The computer makes a move.
                 m = self.strategy.suggest_move(self.state)
-                print('The computer chooses: {}'.format(m))
+                print 'The computer chooses: {}'.format(m)
             self.state = self.state.apply_move(m)
-            print('New game state: ', str(self.state))
-            print()
+            print 'New game state: ', str(self.state)
+            print '\n'
 
         if self.state.winner('p2'):
             # p2, the computer, wins
-            print('You lost -- Try next time')
+            print 'You lost -- Try next time'
         elif self.state.winner('p1'):
             # p1, the human challenger, wins
-            print('Congrats -- you won!!')
+            print 'Congrats -- you won!!'
         else:
-            print('We tied...')
-
+            print 'We tied...'
 
 if __name__ == '__main__':
     from tic_tac_toe_game_state import TTTGameState
@@ -62,5 +62,9 @@ if __name__ == '__main__':
     strategy = ({'r': StrategyRandom, 'm': StrategyMinimax})
     s = ''
     while s not in strategy.keys():
-        s = input('r for random strategy for computer, m for minimax: ')
+        if sys.version[0] == '2':
+            s = raw_input('r for random strategy for computer, m for minimax: ')
+        else:
+            s = input('r for random strategy for computer, m for minimax: ')
+
     GameView(TTTGameState, strategy[s]).play()
