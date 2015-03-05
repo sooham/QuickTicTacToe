d501f2b426e4849e8e02852a49422eed9ddc64b3
Sooham Rafiz <rafizsooham@gmail.com>
2015-03-05 18:28:47 -0500

    Fixed shebang line, added images to README

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ Well here is **Quick Tic-Tac-Toe** just for you!
 
 ###Installation
 _Note: Only Unix systems with preinstalled python2 can play from any directory_
+#####Dependencies:
+    * (Python 3.4+)[https://www.python.org/downloads/]
 ####Unix:
 ``` html
 git clone https://github.com/sooham/QuickTicTacToe
@@ -18,4 +20,4 @@ sudo cp ~/Path/to/cloned/repo/QuickTicTacToe/*.py /usr/local/bin
 
 Then your ready to play awesome Tic-Tac-Toe!
 
-![Image of game]()
+![Image of game](https://github.com/sooham/QuickTicTacToe/img/img1.tiff)
--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python  
-# ^
+#!Library/Frameworks/Python.framework/Versions/3.4/bin/python3
 
 import sys
+
+
 class GameView:
     '''
     A game view for a two-player, sequential move, zero-sum,
@@ -28,35 +29,35 @@ class GameView:
 
         Play a game.
         '''
-        print self.state.instructions
-        print self.state
-        print '\n'
+        print(self.state.instructions)
+        print(self.state)
+        print()
         while self.state.possible_next_moves():
             if self.state.next_player == 'p1':
                 m = self.state.get_move()
                 while m not in self.state.possible_next_moves():
                     # The move was illegal.
-                    print 'Illegal move: {}\nPlease try again.\n'.format(m)
-                    print self.state.instructions
-                    print self.state
+                    print('Illegal move: {}\nPlease try again.\n'.format(m))
+                    print(self.state.instructions)
+                    print(self.state)
                     m = self.state.get_move()
-                print 'You choose: {}'.format(m)
+                print('You choose: {}'.format(m))
             else:
                 # The computer makes a move.
                 m = self.strategy.suggest_move(self.state)
-                print 'The computer chooses: {}'.format(m)
+                print('The computer chooses: {}'.format(m))
             self.state = self.state.apply_move(m)
-            print 'New game state: ', str(self.state)
-            print '\n'
+            print('New game state: ', str(self.state))
+            print()
 
         if self.state.winner('p2'):
             # p2, the computer, wins
-            print 'You lost -- Try next time'
+            print('You lost -- Try next time')
         elif self.state.winner('p1'):
             # p1, the human challenger, wins
-            print 'Congrats -- you won!!'
+            print('Congrats -- you won!!')
         else:
-            print 'We tied...'
+            print('We tied...')
 
 if __name__ == '__main__':
     from tic_tac_toe_game_state import TTTGameState
